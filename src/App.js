1fc5fccea5b83633ea7435718f573be868c6fbd2
Ankush Thakur <ankusht2307@gmail.com>
2022-05-29 21:16:10 +0530

    useRef Implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  // const [renderCount, setRenderCount] = useState(0);
+  const renderCount = useRef(0);
+  // Case 1: Display how many times the component is rendered
+  // ! This will cause infinite renders because we
+  // ! are changing state and then upon that state chnage
+  // ! component is again rendering.
+  useEffect(() => {
+    // setRenderCount((prevCount) => prevCount + 1);
+    renderCount.current = renderCount.current + 1;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* usecase: Persisting values accross the renders 
+      without affecting the state change. */}
+      <h1>useRef</h1>
+      <p>I rendered {renderCount.current} times</p>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </>
   );
 }
 
